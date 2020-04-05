3ee5596e5bd1107f7aeeb33e8b4f5e37bf3aad07
timkay <timkay@not.com>
2020-04-05 12:04:37 -0700

    build executable

---
+++ buildexe.bat
@@ -0,0 +1,4 @@
+
+rem pyinstaller -wF "C:/Users/timkay/Dropbox/MASTR/work/arduino-boards/main.py" -i "C:/Users/timkay/Downloads/arduino.ico" --workpath "C:/Users/timkay/Dropbox/MASTR/work/arduino-boards" --distpath "C:/Users/timkay/Dropbox/MASTR/work/arduino-boards" --specpath "C:/Users/timkay/Dropbox/MASTR/work/arduino-boards"
+
+pyinstaller -wF "arduino-boards.py" -i "arduino.ico"
